1b4449d88cf3b8a04ceea905531b0a56f2fe8a75
Leonardo Nunes <leonunesbs@gmail.com>
2025-07-20 20:42:17 -0300

    feat: add footer with credit link

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,6 +1,7 @@
-import { redirect } from "next/navigation";
+import Link from "next/link";
 import { SignInForm } from "~/components/organisms/signin-form";
 import { auth } from "~/server/auth";
+import { redirect } from "next/navigation";
 
 type SearchParams = Promise<Record<string, string | string[] | undefined>>;
 
@@ -12,8 +13,20 @@ export default async function Login(props: { searchParams: SearchParams }) {
   }
 
   return (
-    <div className="flex h-screen w-full items-center justify-center px-4">
+    <div className="relative flex h-screen w-full items-center justify-center px-4">
       <SignInForm callbackUrl={callbackUrl as string} />
+      <div className="absolute bottom-0 flex w-full">
+        <span className="w-full bg-sidebar-primary py-1 text-center text-xs text-sidebar-primary-foreground">
+          Coded with ❤️ by{" "}
+          <Link
+            href={"https://instagram.com/leonunesbs"}
+            className="link font-bold no-underline"
+            target="_blank"
+          >
+            @leonunesbs
+          </Link>
+        </span>
+      </div>
     </div>
   );
 }
