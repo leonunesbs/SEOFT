3ccd96fc78bd676ac4dad609860731d25937abe3
Leonardo Nunes <leonunesbs@gmail.com>
2025-07-21 20:54:55 -0300

    refactor: replace Drawer with Dialog in ResponsiveDialog for consistent UI handling

--- src/components/ui/responsive-dialog.tsx
+++ src/components/ui/responsive-dialog.tsx
@@ -5,22 +5,12 @@ import * as React from "react";
 import {
   Dialog,
   DialogContent,
+  DialogDescription,
   DialogFooter,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
 } from "~/components/ui/dialog";
-import {
-  Drawer,
-  DrawerContent,
-  DrawerDescription,
-  DrawerFooter,
-  DrawerHeader,
-  DrawerTitle,
-  DrawerTrigger,
-} from "~/components/ui/drawer";
-
-import { useIsMobileOrIOSHome } from "~/hooks/use-mobile";
 
 interface ResponsiveDialogProps {
   trigger: React.ReactNode;
@@ -43,35 +33,13 @@ export function ResponsiveDialog({
   description,
   footer,
 }: ResponsiveDialogProps) {
-  const isMobileOrIOSHome = useIsMobileOrIOSHome();
-
-  if (isMobileOrIOSHome) {
-    return (
-      <Drawer open={open} onOpenChange={onOpenChange}>
-        <DrawerTrigger asChild>{trigger}</DrawerTrigger>
-        <DrawerContent className={className}>
-          <div className="mx-auto w-full max-w-xl">
-            <DrawerHeader>
-              <DrawerTitle>{title}</DrawerTitle>
-              {description && (
-                <DrawerDescription>{description}</DrawerDescription>
-              )}
-            </DrawerHeader>
-            <div className="flex-1 overflow-y-auto px-4 pb-4">{children}</div>
-            {footer && <DrawerFooter>{footer}</DrawerFooter>}
-          </div>
-        </DrawerContent>
-      </Drawer>
-    );
-  }
-
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
       <DialogTrigger asChild>{trigger}</DialogTrigger>
       <DialogContent className={className}>
         <DialogHeader>
           <DialogTitle>{title}</DialogTitle>
-          {description && <DrawerDescription>{description}</DrawerDescription>}
+          {description && <DialogDescription>{description}</DialogDescription>}
         </DialogHeader>
         <div className="max-h-[70vh] overflow-y-auto px-1 pb-4">{children}</div>
         {footer && <DialogFooter>{footer}</DialogFooter>}
